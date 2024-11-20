8112667b0dde5b34deb5989c2ffcc7600f21ed63
LNA7578 <helenaarenard@gmail.com>
2024-11-20 15:05:15 +0100

    Update main.py

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,17 @@
 # Écrivez votre code ici !
+name = " Helena "
+age = 30 
+taille = 1.60
+est_etudiante = True 
+
+# Afficher les valeurs des variables 
+Print ( f " name : { name } " )
+print (f " age : { age }" )
+print ( f " taille : { taille } " )
+print ( f " est_etudiante : { est_etudiante } ") 
+
+# afficher les types de variables 
+print ( f " Type : { type (name ) } " )
+print ( f " Type : { type (age) } " )
+print ( f " Type : { type (taille ) } " )
+print ( f " Type : { type (est_etudiante) } " )
